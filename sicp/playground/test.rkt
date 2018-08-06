c927cb36a472323c3db000138239ba552aefeeda
dvmarinoff <dvmarinoff@gmail.com>
2018-08-06 11:45:55 +0300

    data as procedures

--- sicp/playground/test.rkt
+++ sicp/playground/test.rkt
@@ -50,3 +50,58 @@
   (sum square a inc b))
 
 (sum-sq 1 4)
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+;; points in a plane
+(define (make-vector x y)
+  (cons x y))
+
+(define (xcor p) (car p))
+
+(define (ycor p) (cdr p))
+
+;; line segments
+(define (make-seg p q)
+  (cons p q))
+
+(define (seg-start s) (car s))
+
+(define (seg-end s) (cdr s))
+
+(define (mid-point s)
+  (let ((a (seg-start s))
+        (b (seg-end s)))
+    (make-vector
+      (average (xcor a) (xcor b))
+      (average (ycor a) (ycor b)))))
+
+(define (seg-length s)
+  (let
+      ((dx (- (xcor (seg-end s))
+              (xcor (seg-start s))))
+       (dy (- (ycor (seg-end s))
+              (ycor (seg-start s)))))
+    (sqrt (+ (square dx)
+             (square dy)))))
+
+(define A (make-vector 1 3))
+(define B (make-vector 5 5))
+
+(define AB (make-seg A B))
+
+(mid-point AB)
+
+(define (cons a b)
+  (lambda (pick)
+    (cond ((= pick 1) a)
+          ((= pick 2) b))))
+
+(define (car x) (x 1))
+
+(define (cdr x) (x 2))
+
+(define p (cons 1 3))
+(car p)
+(cdr p)
