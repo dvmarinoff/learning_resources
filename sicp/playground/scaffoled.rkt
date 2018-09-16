f26c769d000564df6c8ca18793fdd3a60e72c045
dvmarinoff <dvmarinoff@gmail.com>
2018-09-16 17:27:38 +0300

    added command line parser to scaffold

--- sicp/playground/scaffoled.rkt
+++ sicp/playground/scaffoled.rkt
@@ -3,10 +3,12 @@
 ;; Scaffolds a new sicp exercise with exercise and test file
 
 ;; My second racket program, so I still try to keep close to
-;; sicp chapter 2 scheme, but I am using @-string exps.
+;; sicp chapter 2 scheme, but I am using @-string exps and
+;; command-line parser from racket.
 
-(define chapter 1)
-(define exercise 1)
+(define chapter (make-parameter 1))
+(define exercise (make-parameter 1))
+(define tests (make-parameter #t))
 
 (define (chapter-format n)
   (format "~a" n))
@@ -32,37 +34,68 @@
 
 ;; @-sting expression
 (define template-tests @string-append{
-(requre rackunit rackunit/text-ui)
+#lang racket
+
+(require rackunit rackunit/text-ui)
 
 (load @(format "\"~a\""
-               (exercise-path-relative-to-test exercise)))
+               (exercise-path-relative-to-test (exercise))))
 
-(define @(format "sicp-~a-tests" (exercise-name chapter exercise))
+(define @(format "sicp-~a-tests" (exercise-name (chapter) (exercise)))
   (test-suite
    @(format "\"testing ~a description\""
-            (exercise-name chapter exercise))
+            (exercise-name (chapter) (exercise)))
 
     (check-equal? (main 0) 0)
 ))
 
 (run-tests @(format "sicp-~a-tests"
-                    (exercise-name chapter exercise)))
+                    (exercise-name (chapter) (exercise))))
 })
+;; end
 
 ;; @-sting expression
 (define template-exercise @string-append{
-;; ex @(format "~a" (exercise-name chapter exercise))
+#lang racket
+;; ex @(format "~a" (exercise-name (chapter) (exercise)))
 ;;
 
+(define (main n) n)
+
 })
+;; end
 
 (define (touch file-name content)
   (define out (open-output-file file-name #:exists 'truncate))
   (display content out)
   (close-output-port out))
 
-(touch (exercise-file-name exercise) template-exercise)
-(touch (test-file-name exercise) template-tests)
+(define parser
+  (command-line
+   #:usage-help
+   "Scaffold creates and fills in with some starter code files for sicp exercises."
+
+   #:once-each
+   [("-c" "--chapter") CHAPTER
+    "the chapter number as an integer"
+    (chapter (string->number CHAPTER))]
+
+   [("-e" "--exercise") EXERCISE
+    "the exercice number as an integer"
+    (exercise (string->number EXERCISE))]
+
+   [("-t" "--tests") TESTS
+    "boolean include a test file or not default is true"
+    (tests (string=? "t" TESTS))]
+
+   #:args () (void)))
+
+(define (main)
+  (touch (exercise-file-name (exercise)) template-exercise)
+  (when (equal? #t (tests))
+    (touch (test-file-name (exercise)) template-tests)))
+
+(main)
 
 ;; (require net/http-client)
 ;; (require html-parsing)
