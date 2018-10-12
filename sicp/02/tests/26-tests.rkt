3612a54512fdbff0f17a537aae49cb39dad42701
dvmarinoff <dvmarinoff@gmail.com>
2018-10-12 22:56:30 +0300

    examine append, cons and list differences

--- sicp/02/tests/26-tests.rkt
+++ sicp/02/tests/26-tests.rkt
@@ -2,13 +2,15 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../26.rkt")
+(load "./26.rkt")
 
 (define sicp-02.26-tests
   (test-suite
    "testing 02.26 examine append, cons and list differences"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (append x y) '(1 2 3 4 5 6))
+   (check-equal? (cons x y) '((1 2 3) 4 5 6))
+   (check-equal? (list x y) '((1 2 3) (4 5 6)))
 ))
 
 (run-tests sicp-02.26-tests)
