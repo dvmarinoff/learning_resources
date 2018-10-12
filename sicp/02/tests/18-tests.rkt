4f14bfbeae68e1a31bd564657bd44b2b631596a1
dvmarinoff <dvmarinoff@gmail.com>
2018-10-12 17:00:44 +0300

    reverse procedure

--- sicp/02/tests/18-tests.rkt
+++ sicp/02/tests/18-tests.rkt
@@ -2,13 +2,16 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../18.rkt")
+(load "./18.rkt")
 
 (define sicp-02.18-tests
   (test-suite
    "testing 02.18 reverse procedure"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (reverse (list 1 4 9 16 25)) (list 25 16 9 4 1))
+   (check-equal? (reverse (list 1 4)) (list 4 1))
+   (check-equal? (reverse (list 1)) (list 1))
+   (check-equal? (reverse '()) '())
 ))
 
 (run-tests sicp-02.18-tests)
