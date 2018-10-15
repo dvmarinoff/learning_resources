74f7c8238d2b93c2cd22ca6ac6f2bdeed71175de
dvmarinoff <dvmarinoff@gmail.com>
2018-10-15 15:15:33 +0300

    deep-reverse procedure

--- sicp/02/27.rkt
+++ sicp/02/27.rkt
@@ -7,11 +7,34 @@
 ;; returns as its value the list with its elements reversed and
 ;; with all sublists deep-reversed as well. For example,
 ;;
-;; (define x (list (list 1 2) (list 3 4)))x
+;; (define x (list (list 1 2) (list 3 4)))
+;; x
 ;; ((1 2) (3 4))
 ;; (reverse x)
 ;; ((3 4) (1 2))
 ;; (deep-reverse x)
 ;; ((4 3) (2 1))
 
-(define (main n) n)
+(define x (list (list 1 2) (list 3 4)))
+
+(define y '((1 2 (3 4 (5 6 7 8) 9) 10)))
+
+(deep-reverse y)
+(deep-sum y)
+
+(define deep-reverse (traverse reverse))
+(define deep-sum (traverse sum-list))
+
+(define (traverse fn)
+  (lambda (xs)
+    (define (recur xs)
+      (if (not (list? xs))
+         xs
+         (fn (map recur xs))))
+    (recur xs)))
+
+(define (reverse lst)
+  (foldl (lambda (a b) (cons a b)) '() lst))
+
+(define (sum-list lst)
+  (foldl + 0 lst))
--- sicp/02/tests/27-tests.rkt
+++ sicp/02/tests/27-tests.rkt
@@ -2,13 +2,57 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../27.rkt")
+(load "./27.rkt")
 
 (define sicp-02.27-tests
   (test-suite
    "testing 02.27 deep-reverse procedure"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (deep-reverse (list (list 1 2)
+                                     (list 3 4)))
+                 '((4 3) (2 1)))
+
+   (check-equal? (deep-reverse (list (list 1 2)
+                                     (list 3 4)
+                                     (list 5 6)))
+                 '((6 5) (4 3) (2 1)))
+
+   (check-equal? (deep-reverse (list (list 1 2)
+                                     (list 3 4 (list 5 6 7))
+                                     (list 8   (list 9 10))))
+                 '(((10 9) 8)
+                   ((7 6 5) 4 3)
+                   (2 1)))
+
+   (check-equal? (deep-reverse (list
+                                (list 1 2
+                                      (list 3 4
+                                            (list 5 6
+                                                  (list 7 8))))))
+                 '(((((8 7) 6 5) 4 3) 2 1)))
+
+   ;; (check-equal? (deep-reverse-low (list (list 1 2)
+   ;;                                   (list 3 4)))
+   ;;               '((4 3) (2 1)))
+
+   ;; (check-equal? (deep-reverse-low (list (list 1 2)
+   ;;                                   (list 3 4)
+   ;;                                   (list 5 6)))
+   ;;               '((6 5) (4 3) (2 1)))
+
+   ;; (check-equal? (deep-reverse-low (list (list 1 2)
+   ;;                                   (list 3 4 (list 5 6 7))
+   ;;                                   (list 8   (list 9 10))))
+   ;;               '(((10 9) 8)
+   ;;                 ((7 6 5) 4 3)
+   ;;                 (2 1)))
+
+   ;; (check-equal? (deep-reverse-low (list
+   ;;                              (list 1 2
+   ;;                                    (list 3 4
+   ;;                                          (list 5 6
+   ;;                                                (list 7 8))))))
+   ;;               '(((((8 7) 6 5) 4 3) 2 1)))
 ))
 
 (run-tests sicp-02.27-tests)
