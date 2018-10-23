ec1e893d857703dd4eb1284ce495e77e9d9c5861
dvmarinoff <dvmarinoff@gmail.com>
2018-10-23 10:56:34 +0300

    abstract tree-map procedure

--- sicp/02/31.rkt
+++ sicp/02/31.rkt
@@ -1,10 +1,18 @@
 #lang racket
 ;; ex 02.31
-;; binary mobile extend-tree procedure
+;; abstract tree-map procedure
 ;;
 ;; Abstract your answer to exercise 2.30 to produce a procedure
 ;; tree-map with the property that square-tree could be defined as
 ;;
 ;; (define (square-tree tree) (tree-map square tree))
 
-(define (main n) n)
+(define (square-tree tree)
+  (tree-map sqr tree))
+
+(define (tree-map fn xs)
+  (map (lambda (x)
+         (if (pair? x)
+             (tree-map fn x)
+             (fn x)))
+       xs))
--- sicp/02/tests/31-tests.rkt
+++ sicp/02/tests/31-tests.rkt
@@ -2,13 +2,19 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../31.rkt")
+(load "./31.rkt")
 
 (define sicp-02.31-tests
   (test-suite
-   "testing 02.31 binary mobile extend-tree procedure"
+   "testing 02.31 abstract tree-map procedure"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (square-tree '()) '())
+   (check-equal? (square-tree '(1)) '(1))
+   (check-equal? (square-tree '(2)) '(4))
+   (check-equal? (square-tree '(1 2 3 4)) '(1 4 9 16))
+   (check-equal? (square-tree '((1 2) (3 4))) '((1 4) (9 16)))
+   (check-equal? (square-tree '((1 2) (3 4 (5 6 (7 8))))) '((1 4) (9 16 (25 36 (49 64)))))
+   (check-equal? (square-tree '((1 2) (3 4 (5 6)) (7 8))) '((1 4) (9 16 (25 36)) (49 64)))
 ))
 
 (run-tests sicp-02.31-tests)
