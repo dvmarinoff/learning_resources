15026b2be95ee74fb26b4618dc315bf9534864e1
dvmarinoff <dvmarinoff@gmail.com>
2018-10-23 16:49:42 +0300

    seq methods with fold (accumulation)

--- sicp/02/tests/33-tests.rkt
+++ sicp/02/tests/33-tests.rkt
@@ -2,13 +2,16 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../33.rkt")
+(load "./33.rkt")
 
 (define sicp-02.33-tests
   (test-suite
-   "testing 02.33 practice accumulaation lambdas"
+   "testing 02.33 seq methods with fold (accumulation)"
 
-    (check-equal? (main 0) 0)
+    (check-equal? (map inc '(1 2 3 4)) '(2 3 4 5))
+    (check-equal? (append '(1 2) '(3 4)) '(1 2 3 4))
+    (check-equal? (length '(1 2 3 4)) 4)
+
 ))
 
 (run-tests sicp-02.33-tests)
