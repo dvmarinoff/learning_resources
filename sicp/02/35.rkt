815d4f4bc6cbedb0d1eb46106d447940b600d59d
dvmarinoff <dvmarinoff@gmail.com>
2018-10-23 17:37:40 +0300

    count-leaves as accumulation

--- sicp/02/35.rkt
+++ sicp/02/35.rkt
@@ -6,4 +6,18 @@
 ;; (define (count-leaves t)
 ;;   (accumulate <??> <??> (map <??> <??>)))
 
-(define (main n) n)
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x)
+                         (if (pair? x)
+                             (count-leaves x)
+                             1)) t)))
+
+;; Accumulate + over 0 with the recursive map over the sub-trees.
+;; Instead of car and cdr you descent with a recursive call to
+;; the main function
+;;
+;; (define (count-leaves-old x)
+;;   (cond ((null? x) 0)
+;;         ((not (pair? x)) 1)
+;;         (else (+ (count-leaves (car x))
+;;                  (count-leaves (cdr x))))))
--- sicp/02/tests/35-tests.rkt
+++ sicp/02/tests/35-tests.rkt
@@ -2,13 +2,17 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../35.rkt")
+(load "./35.rkt")
 
 (define sicp-02.35-tests
   (test-suite
    "testing 02.35 count-leaves as accumulation"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (count-leaves '()) 0)
+   (check-equal? (count-leaves '(0)) 1)
+   (check-equal? (count-leaves '(1 2 3 4)) 4)
+   (check-equal? (count-leaves '((1 2) (3 4))) 4)
+   (check-equal? (count-leaves '((1 2) (3 4 (5 6 (7 8))))) 8)
 ))
 
 (run-tests sicp-02.35-tests)
