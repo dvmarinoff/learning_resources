8d43497e31bea0343c7a444ab9e8799b0fc77fca
dvmarinoff <dvmarinoff@gmail.com>
2018-10-24 15:51:37 +0300

    find ordered triples <= n that sum to s

--- sicp/02/41.rkt
+++ sicp/02/41.rkt
@@ -1,9 +1,45 @@
 #lang racket
 ;; ex 02.41
-;; ordered pairs of distinct positive integers
+;; find ordered triples <= n that sum to s
 ;;
 ;; Write a procedure to find all ordered triples of distinct
 ;; positive integers i, j, and k less than or equal to a given
 ;; integer n that sum to a given integer s.
 
-(define (main n) n)
+;;;;
+;; solution
+;;;;
+(define (ordered-triples-sum n s)
+  (filter (curry sum-to? s) (unique-triples n)))
+
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda(j)
+                    (map (lambda (k)
+                           (list i j k))
+                         (enumerate-interval 1 (- j 1))))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (sum-to? s xs)
+  (equal? s (sum xs)))
+
+(define (sum xs)
+  (foldl + 0 xs))
+
+;;;;
+;; required code
+;;;;
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
--- sicp/02/tests/41-tests.rkt
+++ sicp/02/tests/41-tests.rkt
@@ -6,9 +6,19 @@
 
 (define sicp-02.41-tests
   (test-suite
-   "testing 02.41 ordered pairs of distinct positive integers"
+   "testing 02.41 find ordered triples <= n that sum to s"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (ordered-triples-sum 3 6) '((3 2 1)))
+
+   (check-equal? (unique-triples 3) '((3 2 1)))
+
+   (check-true (sum-to? 0 '()))
+   (check-true (sum-to? 1 '(1)))
+   (check-true (sum-to? 10 '(1 2 3 4)))
+
+   (check-equal? (sum '()) 0)
+   (check-equal? (sum '(1)) 1)
+   (check-equal? (sum '(1 2 3 4)) 10)
 ))
 
 (run-tests sicp-02.41-tests)
