9325b3274ca03e099e497234be12303c89607a1a
dvmarinoff <dvmarinoff@gmail.com>
2018-10-27 20:05:49 +0300

    evaluate some quoted expressions

---
+++ sicp/02/53.rkt
@@ -0,0 +1,21 @@
+#lang racket
+;; ex 02.53
+;; evaluate some quoted expressions
+;;
+;; What would the interpreter print in response to evaluating
+;; each of the following expressions?
+;;
+(list 'a 'b 'c)
+;; -> '(a b c)
+(list (list 'george))
+;; -> '((george))
+(cdr '((x1 x2) (y1 y2)))
+;; -> '((y1 y2))
+(cadr '((x1 x2) (y1 y2)))
+;; -> '(y1 y2)
+(pair? (car '(a short list)))
+;; -> #f
+(memq 'red '((red shoes) (blue socks)))
+;; -> #f
+(memq 'red '(red shoes blue socks))
+;; -> '(red shoes blue socks)
