a144358e80bab27c458033fe586a763f6052b160
dvmarinoff <dvmarinoff@gmail.com>
2018-10-27 15:28:10 +0300

    picture language 2d vectors

--- sicp/02/46.rkt
+++ sicp/02/46.rkt
@@ -15,4 +15,27 @@
 ;;(x_{1}, y_{1}) - (x_{2}, y_{2}) = (x_{1} - x_{2}, y_{1} - y_{1})
 ;;s * (x, y) = (sx, sy)
 
-(define (main n) n)
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1)
+                (xcor-vect v2))
+             (+ (ycor-vect v1)
+                (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1)
+                (xcor-vect v2))
+             (- (ycor-vect v1)
+                (ycor-vect v2))))
+
+(define (scale-vect scalar vect)
+  (make-vect (* scalar (xcor-vect vect))
+             (* scalar (xcor-vect vect))))
--- sicp/02/tests/46-tests.rkt
+++ sicp/02/tests/46-tests.rkt
@@ -2,13 +2,22 @@
 
 (require rackunit rackunit/text-ui)
 
-(load "../46.rkt")
+(load "./46.rkt")
 
 (define sicp-02.46-tests
   (test-suite
    "testing 02.46 picture language 2d vectors"
 
-    (check-equal? (main 0) 0)
+   (check-equal? (add-vect (make-vect 2 2)
+                           (make-vect 4 2))
+                 (make-vect 6 4))
+
+   (check-equal? (sub-vect (make-vect 2 2)
+                           (make-vect 4 2))
+                 (make-vect -2 0))
+
+   (check-equal? (scale-vect 2 (make-vect 2 2))
+                 (make-vect 4 4))
 ))
 
 (run-tests sicp-02.46-tests)
