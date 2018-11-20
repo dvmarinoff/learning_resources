638a7bb93effdc7dc4f9e94d273f83353b596834
dvmarinoff <dvmarinoff@gmail.com>
2018-11-20 13:23:13 +0200

    add call-the-cops feature to make-account

--- sicp/03/tests/04-tests.rkt
+++ sicp/03/tests/04-tests.rkt
@@ -8,7 +8,14 @@
   (test-suite
    "testing 03.04 add call-the-cops feature to make-account"
 
-    (check-equal? (main 0) 0)
+    (check-equal? ((acc 'secret-password 'withdraw) 40) 60)
+    (check-equal? ((acc 'secret-password 'withdraw) 120)
+                  "Insufficient funds.")
+    (check-equal? ((acc 'secret-password 'deposit) 40) 100 "deposit")
+
+    (check-equal? ((acc 'wrong-password 'withdraw) 40)
+                  "Incorrect password."
+                  "pass fail")
 ))
 
 (run-tests sicp-03.04-tests)
