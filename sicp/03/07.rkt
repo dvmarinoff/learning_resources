2eb4aa5951e21ce3f7a7740f8039f1312f0c972d
dvmarinoff <dvmarinoff@gmail.com>
2018-11-20 20:16:08 +0200

    make-joint account procedure

--- sicp/03/07.rkt
+++ sicp/03/07.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;; ex 03.07
-;; make-joint procedure
+;; make-joint account procedure
 ;;
 ;; Consider the bank account objects created by make-account,
 ;; with the password modification described in exercise 3.3.
@@ -22,8 +22,35 @@
 ;; name paul-acc and the password rosebud. You may wish to modify
 ;; your solution to exercise 3.3 to accommodate this new feature.
 
-(define (make-joint acc account user)
-  ())
+(define (make-joint account owner-password joint-password)
+  (lambda (password method)
+    (cond ((eq? password joint-password)
+           (account owner-password method))
+          (else (lambda (x) "Incorrect password.")))))
+
+(define peter-acc (make-account 100 'open-sesame))
 
 (define paul-acc
   (make-joint peter-acc 'open-sesame 'rosebud))
+
+;; ((peter-acc 'open-sesame 'deposit) 10)
+;; ((paul-acc 'rosebud 'deposit) 10)
+;; ((peter-acc 'open-sesame 'withdraw) 10)
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds."))
+  (define (deposit amount)
+    (begin (set! balance (+ balance amount))
+           balance))
+  (define (auth? pass)
+    (eq? password pass))
+  (define (dispatch pass method)
+    (cond ((not (auth? pass)) (lambda (n) "Incorrect password."))
+          ((eq? method 'withdraw) withdraw)
+          ((eq? method 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT" method))))
+  dispatch)
--- sicp/03/tests/07-tests.rkt
+++ sicp/03/tests/07-tests.rkt
@@ -4,11 +4,18 @@
 
 (load "../07.rkt")
 
+
 (define sicp-03.07-tests
   (test-suite
-   "testing 03.07 make-joint procedure"
+   "testing 03.07 make-joint account procedure"
 
-    (check-equal? (main 0) 0)
+    (test-begin
+     (let* ((peter-acc (make-account 100 'open-sesame))
+            (paul-acc (make-joint peter-acc 'open-sesame 'rosebud)))
+       ((peter-acc 'rosebud 'deposit) 10)
+       ((paul-acc 'rosebud 'deposit) 10)
+       ((peter-acc 'rosebud 'withdraw) 10)
+       (check-equal? ((peter-acc 'open-sesame 'deposit) 0) 110)))
 ))
 
 (run-tests sicp-03.07-tests)
