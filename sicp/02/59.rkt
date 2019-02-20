398ad5a9954fba6b96a59f50c60008990fb2f5e5
dvmarinoff <dvmarinoff@gmail.com>
2019-02-20 11:39:24 +0200

    sets as unordered-lists union-set

--- sicp/02/59.rkt
+++ sicp/02/59.rkt
@@ -5,15 +5,35 @@
 ;; Implement the union-set operation for the unordered-list
 ;; representation of sets.
 
+;; Note:
+;; first implementation preserves order, but does not remove
+;; duplicates from both sets, just from one set:
+;; (union-set (1) (1 2 2))
+;; must be (1 2), but it is (1 2 2)
 (define (union-set set1 set2)
-  (foldr (lambda (x acc) (cons x acc)) set2 set1))
+  (foldr (lambda (x acc) (adjoin-set x acc)) set2 set1))
 
-(define (union-set2 set1 set2)
+;; does not preserve order (as is by requirement) and
+;; works on sets with duplication
+(define (union-set set1 set2)
   (define (recur set1 set2 u)
     (cond ((and (null? set1) (null? set2)) u)
           ((pair? set2)
-           (recur set1 (cdr set2) (cons (car set2) u)))
+           (recur set1 (cdr set2) (adjoin-set (car set2) u)))
           ((pair? set1)
-           (recur (cdr set1) set2 (cons (car set1) u)))))
+           (recur (cdr set1) set2 (adjoin-set (car set1) u)))))
   (recur set1 set2 '()))
 
+
+;;;;
+;; Required code
+;;;;
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? (car set) x) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set x set)
+      set
+      (cons x set)))
--- sicp/02/tests/59-tests.rkt
+++ sicp/02/tests/59-tests.rkt
@@ -9,8 +9,12 @@
    "testing 02.59 sets as unordered-lists union-set"
 
     (check-equal? (union-set '(1) '(2)) '(1 2))
-    (check-equal? (union-set '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
-    (check-equal? (union-set '(1 2 (3 4)) '(5 6 7)) '(1 2 (3 4) 5 6 7))
+    (check-equal? (union-set '(1 2 3) '(4 5 6)) '(3 2 1 6 5 4))
+    (check-equal? (union-set '(1 2 (3 4)) '(5 6 7)) '((3 4) 2 1 7 6 5))
+    (check-equal? (union-set '(1) '(1 2)) '(2 1))
+    (check-equal? (union-set '(1) '(1 2 2)) '(2 1))
+    (check-equal? (union-set '(1 1) '(2)) '(1 2))
+    (check-equal? (union-set '(1 2 3 3) '(4 4 5 6)) '(3 2 1 6 5 4))
 ))
 
 (run-tests sicp-02.59-tests)
