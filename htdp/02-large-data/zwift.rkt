d3dc452f578f601379967407170cf437a0294d41
dvmarinoff <dvmarinoff@gmail.com>
2019-05-14 13:28:33 +0300

    restructuring folders

--- htdp/02-large-data/zwift.rkt
+++
@@ -1,1 +0,0 @@
-adb shell "while true; do screenrecord --output-format=h264 --size 1280x720 -; done" | ffplay -framerate 60 -probesize 32 -vf scale=iw*2:ih -sync video -
--- learning_cs/6.006/merge_sort.rb
+++
@@ -1,6 +0,0 @@
-
-
-
-def merge_sort(xs)
-
-end
